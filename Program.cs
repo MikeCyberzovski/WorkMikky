0572bd33e7496eb05ac1a275249420ea3c35e58c
MikeCyberzovski <135357152+MikeCyberzovski@users.noreply.github.com>
2024-02-09 15:17:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgrammMikky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "Hello", "World", "Hello", "Again" };
+
+            string[] shortStrings = input
+            .Where(s => s.Length <=3)
+            .ToArray();
+
+            Console.WriteLine("Короткие строки:");
+            foreach (string s in shortStrings)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+            
+
+            
+
